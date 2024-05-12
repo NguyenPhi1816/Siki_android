314d76f0acb6fa58684d701104ed13dcf332a0eb
Thuan ngo <thuanngo3072002@gmail.com>
2024-05-12 20:31:53 +0700

    edit title of modal dialog

--- app/src/main/java/com/example/siki/database/OrderDataSource.java
+++ app/src/main/java/com/example/siki/database/OrderDataSource.java
@@ -10,12 +10,18 @@ import android.os.Build;
 
 import com.example.siki.enums.OrderStatus;
 import com.example.siki.model.Order;
+import com.example.siki.model.Product;
 import com.example.siki.model.User;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 public class OrderDataSource {
@@ -81,11 +87,15 @@ public class OrderDataSource {
                 order.setReceiverName(cursor.getString(3));
                 order.setNote(cursor.getString(4));
                 String createdAt = cursor.getString(5);
-                /*DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-                // Parse the string to LocalDateTime
-                LocalDateTime localDateTime = LocalDateTime.parse(createdAt, formatter);*/
-                order.setCreatedAt(LocalDateTime.of(2024,12,12, 12,12));
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss 'GMT'Z yyyy", Locale.US);
+                Date date = null;
+                try {
+                    date = dateFormat.parse(createdAt);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+                order.setCreatedAt(localDateTime);
                 order.setStatus(OrderStatus.valueOf(cursor.getString(6)));
                 order.setOrderDetails(orderDetailDatasource.findByOrderId(orderId, productDatabase));
                 int userId = cursor.getInt(7);
@@ -124,6 +134,44 @@ public class OrderDataSource {
         }
         return Optional.empty();
     }
+    @SuppressLint("NewApi")
+    public List<Order> findAllByUserId(UserDataSource userDataSource,
+                               ProductDatabase productDatabase,
+                               OrderDetailDatasource orderDetailDatasource,
+                                       int userId) {
+        String sql = "Select * from `Order` Where user_id = ?";
+        List<Order> orders = new ArrayList<>();
+        Cursor cursor = db.rawQuery(sql, new String[]{String.valueOf(userId)});
+        if (cursor.moveToFirst()) {
+            do {
+                Order order = new Order();
+                Long orderId = cursor.getLong(0);
+                order.setId(orderId);
+                order.setReceiverPhoneNumber(cursor.getString(1));
+                order.setReceiverAddress(cursor.getString(2));
+                order.setReceiverName(cursor.getString(3));
+                order.setNote(cursor.getString(4));
+                String createdAt = cursor.getString(5);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss 'GMT'Z yyyy", Locale.US);
+                Date date = null;
+                try {
+                    date = dateFormat.parse(createdAt);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+                order.setCreatedAt(localDateTime);
+                order.setStatus(OrderStatus.valueOf(cursor.getString(6)));
+                order.setOrderDetails(orderDetailDatasource.findByOrderId(orderId, productDatabase));
+                User user = userDataSource.getUserById(cursor.getInt(7));
+                order.setUser(user);
+                orders.add(order);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return orders;
+    }
+
 
     public Long createOrder (String receiverPhoneNumber,
                              String receiverAddress,
@@ -154,11 +202,21 @@ public class OrderDataSource {
         int rowsAffected = -1;
         try {
             ContentValues values = new ContentValues();
+            values.put("note", order.getNote());
             values.put("receiverName", order.getReceiverName());
             values.put("receiverPhoneNumber", order.getReceiverPhoneNumber());
             values.put("receiverAddress", order.getReceiverAddress());
             values.put("status", order.getStatus().toString());
             rowsAffected = db.update("`Order`", values, "Id=?", new String[]{String.valueOf(id)});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rowsAffected;
+    }
+    public int deleteOrder(Long orderId) {
+        int rowsAffected = -1;
+        try {
+            rowsAffected = db.delete("`Order`", "Id=?", new String[]{String.valueOf(orderId)});
         } catch (Exception e) {
             e.printStackTrace();
         }
